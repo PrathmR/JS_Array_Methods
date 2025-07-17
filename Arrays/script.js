dc03f9681bbad10e80728798360448431356df67
PrathmR <prathmeshredekar16@gmail.com>
2025-07-17 07:19:49 +0530

    commiting js code

--- Arrays/script.js
+++ Arrays/script.js
@@ -1,1 +1,5 @@
 //Create an array of student names and print each
+let arr = ['Jonas', 'Martha', 'Mikkel', 'Helge', 'Claudia'];
+let print_Arr = arr.forEach(function (val) {
+    console.log(val);
+});

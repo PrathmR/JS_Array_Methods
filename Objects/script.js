1ec2993ff62d9dee068e7a0f09713abd995195cb
PrathmR <prathmeshredekar16@gmail.com>
2025-07-18 10:28:54 +0530

    Pushing the objects section

---
+++ Objects/script.js
@@ -0,0 +1,7 @@
+/*Objects are structured state – perfect for modeling anything complex: a user, a form, a product,
+ etc.
+ Use destructuring, chaining, and dynamic keys wisely */
+
+
+// Create an object for a book (title, author, price)
+ 

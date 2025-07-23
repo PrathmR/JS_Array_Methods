7c954edf2aa3aedd650d79435d64d51b06e1a0ed
PrathmR <prathmeshredekar16@gmail.com>
2025-07-23 07:48:21 +0530

    Pushing html and js

--- Event-Handling/script.js
+++ Event-Handling/script.js
@@ -1,3 +1,4 @@
+//Click Event
 //Objective: When the button is clicked, change the text of the paragraph to "Button Clicked!".
 let actionbtn = document.querySelector("#actionBtn");
 let p = document.querySelector("#statusText");
@@ -6,6 +7,7 @@ actionbtn.addEventListener('click', function () {
     
 });
 
+//Input Event
 //As a user types into the input field, display their text in the paragraph below in real-time.
 let textinp = document.querySelector('#username');
 let textout = document.querySelector('#liveText');
@@ -13,3 +15,14 @@ let textout = document.querySelector('#liveText');
 textinp.addEventListener('input', function (input) {
     textout.textContent = input.target.value;
 })
+
+//Change event
+//When the user selects a new option from the dropdown menu, update the paragraph text to show which color they chose.
+let sel = document.querySelector('#colorSelect');
+let choiceText = document.querySelector('#choiceText');
+
+sel.addEventListener('change', function (sel) {
+    choiceText.textContent = sel.target.value;
+
+    
+});

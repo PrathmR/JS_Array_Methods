cca77bc26d7d91f239a545c41a72a2c71c03ac03
PrathmR <prathmeshredekar16@gmail.com>
2025-07-23 07:43:03 +0530

    Pushing html and js

--- Event-Handling/script.js
+++ Event-Handling/script.js
@@ -5,3 +5,11 @@ actionbtn.addEventListener('click', function () {
     p.textContent = "Button Clicked";
     
 });
+
+//As a user types into the input field, display their text in the paragraph below in real-time.
+let textinp = document.querySelector('#username');
+let textout = document.querySelector('#liveText');
+
+textinp.addEventListener('input', function (input) {
+    textout.textContent = input.target.value;
+})

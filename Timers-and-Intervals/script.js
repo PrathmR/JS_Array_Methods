cd21c8a400f21a930f37101b8b7401d3daff1a2c
PrathmR <prathmeshredekar16@gmail.com>
2025-07-24 15:28:03 +0530

    Pushing timera and intervals section

---
+++ Timers-and-Intervals/script.js
@@ -0,0 +1,9 @@
+//Create an setInterval for the downloading progress bar
+
+// --- Your JavaScript using setInterval goes here ---
+
+        // Hint: Get the elements you need to update
+        const progressBarInner = document.getElementById('progress-bar-inner');
+        const progressPercentage = document.getElementById('progress-percentage');
+
+        // Now, it's your turn to write the setInterval logic!

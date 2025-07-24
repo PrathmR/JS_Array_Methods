ced910c85f4bb4565a36c7926d8bd9095fbb7730
PrathmR <prathmeshredekar16@gmail.com>
2025-07-24 12:16:13 +0530

    updating js

--- Froms-and-form-validation/script.js
+++ Froms-and-form-validation/script.js
@@ -1,20 +1,20 @@
-let nm = document.querySelector('#name');
-let email = document.querySelector('#email');
-let password = document.querySelector('#password');
-let form = document.querySelector('form');
+let nm = document.querySelector("#name");
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
+let form = document.querySelector("form");
 
 form.addEventListener("submit", function (dets) {
-    dets.preventDefault();
-    const nameRegex = /^[A-Za-z\s]{2,}$/;
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-    nameRegex.test(nm.value);
-    emailRegex.test(email.value);
-    passwordRegex.test(password.value);
-
-    if (nm.value.length <= 2) {
-        document.querySelector('#nameerror').style.display = 'initial';
-    }
+  dets.preventDefault();
+  const nameRegex = /^[A-Za-z\s]{2,}$/;
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  let nameans = nameRegex.test(nm.value);
+  let emailans = emailRegex.test(email.value);
+  let passaans = passwordRegex.test(password.value);
 
 
-})
+  if (!nameans) {
+    document.querySelector("#nameerror").style.display = "initial";
+  }
+});

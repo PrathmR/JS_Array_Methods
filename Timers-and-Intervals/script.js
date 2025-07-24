407f0b34fcc3c8a01f59d92df2eab814aab67d1f
PrathmR <prathmeshredekar16@gmail.com>
2025-07-24 15:43:05 +0530

    Pushing html and js

--- Timers-and-Intervals/script.js
+++ Timers-and-Intervals/script.js
@@ -1,9 +1,16 @@
-//Create an setInterval for the downloading progress bar
+//Create an setInterval for the downloading progress bar which should end in 5 seconds
+let count = 0;
+const progressBarInner = document.getElementById("progress-bar-inner");
+const progressPercentage = document.getElementById("progress-percentage");
 
-// --- Your JavaScript using setInterval goes here ---
-
-        // Hint: Get the elements you need to update
-        const progressBarInner = document.getElementById('progress-bar-inner');
-        const progressPercentage = document.getElementById('progress-percentage');
-
-        // Now, it's your turn to write the setInterval logic!
+setInterval(function () {
+    if (count <= 99) {
+        count++
+        progressBarInner.style.width =   count + '%'
+        progressPercentage.textContent = `${count}`
+    }
+    if (count === 100) {
+        progressBarInner.style.backgroundColor = 'green';
+    }
+    
+}, 5000 / 100);

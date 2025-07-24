18a34204d57554e979f96cf8c8cf9cf594dafe71
PrathmR <prathmeshredekar16@gmail.com>
2025-07-24 12:56:15 +0530

    updating html and js

--- Froms-and-form-validation/script.js
+++ Froms-and-form-validation/script.js
@@ -4,10 +4,11 @@ let password = document.querySelector("#password");
 let form = document.querySelector("form");
 
 form.addEventListener("submit", function (dets) {
-    dets.preventDefault();
-    document.querySelector("#nameerror").textContent = "";
-    document.querySelector("#emailerror").style.display = "";
-    document.querySelector("#passworderror").style.display = "";
+  dets.preventDefault();
+  document.querySelector("#nameerror").textContent = "";
+  document.querySelector("#emailerror").style.display = "";
+  document.querySelector("#passworderror").style.display = "";
+  document.querySelector("#resultmsg").textContent = "";
   const nameRegex = /^[A-Za-z\s]{3,}$/;
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   const passwordRegex =
@@ -16,20 +17,25 @@ form.addEventListener("submit", function (dets) {
   let emailans = emailRegex.test(email.value);
   let passaans = passwordRegex.test(password.value);
 
-
+  let isvalid = true;
   if (!nameans) {
-      document.querySelector("#nameerror").textContent = "Enter a valid name";
-      document.querySelector("#nameerror").style.display= 'inital';
-      
+    document.querySelector("#nameerror").textContent = "Enter a valid name";
+    document.querySelector("#nameerror").style.display = "initia";
+    isvalid = false;
   }
   if (!emailans) {
-      document.querySelector("#emailerror").style.display = "Enter a valid email";
-       document.querySelector("#emailerror").style.display = "inital";
-      
+    document.querySelector("#emailerror").style.display = "Enter a valid email";
+    document.querySelector("#emailerror").style.display = "initial";
+    isvalid = false;
   }
   if (!passaans) {
-      document.querySelector("#passworderror").style.display = "Choose a strong password";
-       document.querySelector("#passworderror").style.display = "inital";
-      
+    document.querySelector("#passworderror").style.display =
+      "Choose a strong password";
+    document.querySelector("#passworderror").style.display = "initial";
+    isvalid = false;
+  }
+  if (isvalid) {
+    document.querySelector("#resultmsg").textContent =
+      "Submitted Successfully!!";
   }
 });

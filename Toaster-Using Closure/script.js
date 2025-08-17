27a2fe9088387473c4432dc83d20d913d6e6b6ae
PrathmR <prathmeshredekar16@gmail.com>
2025-08-17 16:47:37 +0530

    deleting html

--- Toaster-Using Closure/script.js
+++ Toaster-Using Closure/script.js
@@ -1,29 +1,37 @@
 function createToaster(config) {
-    return function (str) {
-        let div = document.createElement('div');
-        div.textContent = str;
-        div.className = `inline-block ${config.theme === 'dark' ? 'bg-gray-800 text-white' : 'bg-white text-black'} px-6 py-3 rounded shadow-lg pointer-events-none`;
-        document.querySelector('.parent').appendChild(div);
+  return function (str) {
+    let div = document.createElement("div");
+    div.textContent = str;
+    div.className = `inline-block ${
+      config.theme === "dark"
+        ? "bg-gray-800 text-white"
+        : "bg-gray-100 text-black"
+    } px-6 py-3 rounded shadow-lg pointer-events-none`;
 
-        if ( config.positionX !== 'left' || positionY !== 'top') {
-           document.querySelector('.parent').className += `$config.positionX==='right' ? 'right-5' : 'left-5'} ${config.positionY==='bottom' ? 'bottom -5' : 'top-5'}   `;
-        } 
-        setTimeout(() => {
-            document.querySelector('.parent').removeChild(div);
-                
-            },config.duration*3000)
-        
+    document.querySelector(".parent").appendChild(div);
+
+    if (config.positionX !== "left" || positionY !== "top") {
+      document.querySelector(".parent").className += ` ${
+        config.positionX === "right" ? " right-5" : " left-5"
+      } ${config.positionY === "bottom" ? " bottom-5" : " top-5"}`;
     }
-
-    
+    setTimeout(() => {
+      document.querySelector(".parent").removeChild(div);
+    }, config.duration * 1000);
+  };
 }
 
-let toaster = createToaster();
-toaster({
-    positionX:'right',
-    positionY: 'top',
-    theme: 'dark',
-    duration: 3,
+let toaster = createToaster({
+  positionX: "right",
+  positionY: "bottom",
+  theme: "light",
+  duration: 3,
+});
+toaster("Download Done");
+setTimeout(() => {
+  toaster("Harsh accepted your request");
+}, 2000);
 
-})
-toaster('This is a toast message!');
+setTimeout(() => {
+  toaster("Rupesh send you a message");
+}, 1500);

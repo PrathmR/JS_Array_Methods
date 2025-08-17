42b7e09e3240dcf58aae452a72d1d9e6306b66a4
PrathmR <prathmeshredekar16@gmail.com>
2025-08-17 16:39:03 +0530

    creating toaster notification

--- Toaster-Using Closure/script.js
+++ Toaster-Using Closure/script.js
@@ -2,8 +2,17 @@ function createToaster(config) {
     return function (str) {
         let div = document.createElement('div');
         div.textContent = str;
-        div.className = `inline-block ${config.theme==='dark'? 'bg-gray-800 text-white' :'bg-white text-black'} px-6 py-3 rounded shadow-lg pointer-events-none`;
-        document.querySelector('.parent')
+        div.className = `inline-block ${config.theme === 'dark' ? 'bg-gray-800 text-white' : 'bg-white text-black'} px-6 py-3 rounded shadow-lg pointer-events-none`;
+        document.querySelector('.parent').appendChild(div);
+
+        if ( config.positionX !== 'left' || positionY !== 'top') {
+           document.querySelector('.parent').className += `$config.positionX==='right' ? 'right-5' : 'left-5'} ${config.positionY==='bottom' ? 'bottom -5' : 'top-5'}   `;
+        } 
+        setTimeout(() => {
+            document.querySelector('.parent').removeChild(div);
+                
+            },config.duration*3000)
+        
     }
 
     
@@ -17,3 +26,4 @@ toaster({
     duration: 3,
 
 })
+toaster('This is a toast message!');
